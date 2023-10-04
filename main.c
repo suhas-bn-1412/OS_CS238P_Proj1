d9530c00fdcd45d34fbf0c6e5dd97ccd6944fe57
suhas-bn-1412 <suhasbn1412@gmail.com>
2023-10-04 03:35:03 +0000

    Generate out.c based on dag for given operators

--- main.c
+++ main.c
@@ -13,10 +13,51 @@
 
 /* export LD_LIBRARY_PATH=. */
 
+int varId = 0;
+
+int genFuncBodyFromDag(const struct parser_dag *dag, FILE *file) {
+        if (dag) {
+                int leftVarId = genFuncBodyFromDag(dag->left, file);
+                int rightVarId = genFuncBodyFromDag(dag->right, file);
+
+                /* based on type of the node, generate the code */
+                switch (dag->op) {
+                        case PARSER_DAG_:
+                                return -1;
+                        case PARSER_DAG_VAL:
+                                fprintf(file, "t%d = %f;\n", varId, dag->val);
+                                break;
+                        case PARSER_DAG_NEG:
+                                fprintf(file, "t%d = -1 * t%d;\n", varId, rightVarId);
+                                break;
+                        case PARSER_DAG_MUL:
+                                fprintf(file, "t%d = t%d * t%d;\n", varId, leftVarId, rightVarId);
+                                break;
+                        case PARSER_DAG_DIV:
+                                fprintf(file, "t%d = t%d %% t%d;\n", varId, leftVarId, rightVarId);
+                                break;
+                        case PARSER_DAG_ADD:
+                                fprintf(file, "t%d = t%d + t%d;\n", varId, leftVarId, rightVarId);
+                                break;
+                        case PARSER_DAG_SUB:
+                                fprintf(file, "t%d = t%d - t%d;\n", varId, leftVarId, rightVarId);
+                                break;
+                }
+                varId++;
+                return (varId - 1);
+        }
+        return -1;
+}
+
 static void
 generate(const struct parser_dag *dag, FILE *file)
 {
 	/* YOUR CODE HERE */
+        int valueVarId;
+        fprintf(file, "double evaluate(void) {\n");
+        valueVarId = genFuncBodyFromDag(dag, file);
+        fprintf(file, "return t%d;\n", valueVarId);
+        fprintf(file, "}\n");
 }
 
 typedef double (*evaluate_t)(void);
@@ -24,11 +65,13 @@ typedef double (*evaluate_t)(void);
 int
 main(int argc, char *argv[])
 {
-	const char *SOFILE = "out.so";
 	const char *CFILE = "out.c";
 	struct parser *parser;
+	/*
+        const char *SOFILE = "out.so";
 	struct jitc *jitc;
 	evaluate_t fnc;
+        */
 	FILE *file;
 
 	/* usage */
@@ -57,15 +100,17 @@ main(int argc, char *argv[])
 
 	/* JIT compile */
 
+        /*
 	if (jitc_compile(CFILE, SOFILE)) {
 		file_delete(CFILE);
 		TRACE(0);
 		return -1;
 	}
 	file_delete(CFILE);
+        */
 
 	/* dynamic load */
-
+        /*
 	if (!(jitc = jitc_open(SOFILE)) ||
 	    !(fnc = (evaluate_t)jitc_lookup(jitc, "evaluate"))) {
 		file_delete(SOFILE);
@@ -74,11 +119,12 @@ main(int argc, char *argv[])
 		return -1;
 	}
 	printf("%f\n", fnc());
-
+        */
 	/* done */
-
+        /*
 	file_delete(SOFILE);
 	jitc_close(jitc);
+        */
 	return 0;
 }
 
