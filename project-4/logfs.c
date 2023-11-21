af4a3a583c90e12d2a247b41608244d5ebc4aaf2
suhas-bn-1412 <suhasbn1412@gmail.com>
2023-11-21 00:40:14 +0000

    Added logfs append logic
    Changed the logfs definition to contain
    block size as one of the property

--- project-4/logfs.c
+++ project-4/logfs.c
@@ -40,12 +40,17 @@ struct logfs {
 
         void *cur_blk;
         void *cur_off;
+        uint64_t blk_sz;
+        uint64_t available;
 };
 
 void write_to_device(struct logfs *logfs) {
+        assert( !logfs );
 }
 
 void write_to_queue(struct logfs *logfs, void *buf) {
+        assert( !logfs );
+        assert( !buf );
 }
 
 struct logfs *logfs_open(const char *pathname) {
@@ -60,11 +65,12 @@ struct logfs *logfs_open(const char *pathname) {
                 return NULL;
         }
 
-        logfs->write_buf.buf = malloc(WCACHE_BLOCKS * logfs->device->block);
+        logfs->blk_sz = device_block(logfs->device);
+        logfs->write_buf.buf = malloc(WCACHE_BLOCKS * logfs->blk_sz);
         logfs->write_buf.head = logfs->write_buf.buf;
         logfs->write_buf.tail = logfs->write_buf.buf;
 
-        logfs->cur_blk = malloc(logfs->device->block);
+        logfs->cur_blk = malloc(logfs->blk_sz);
         logfs->cur_off = logfs->cur_blk;
 
         /**
@@ -93,4 +99,30 @@ void logfs_close(struct logfs* logfs) {
         free(logfs->cur_blk);
 
         free(logfs);
+}
+
+int logfs_append(struct logfs *logfs, const void *buf, uint64_t len) {
+        uint64_t left_to_write;
+        void *buf_;
+
+        buf_ = (void*)buf;
+        left_to_write = len;
+
+        while(left_to_write > logfs->available) {
+                memcpy(logfs->cur_off, buf_, logfs->available);
+                write_to_queue(logfs, logfs->cur_blk);
+
+                left_to_write -= logfs->available;
+                buf_ = (void*)((char*)buf_ + logfs->available);
+
+                memset(logfs->cur_blk, 0, logfs->blk_sz);
+                logfs->available = logfs->blk_sz;
+                logfs->cur_off = logfs->cur_blk;
+        }
+
+        memcpy(logfs->cur_off, buf_, left_to_write);
+        logfs->cur_off = (void*)((char*)logfs->cur_off + left_to_write);
+        logfs->available -= left_to_write;
+
+        return 0;
 }
